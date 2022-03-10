d4fd2bdbd83860dd5719da234f57cf37cf3a55cd
dillonsmithson <dillon.smithson1@gmail.com>
2022-03-10 12:22:51 -0600

    Added file that contains SQL statements for Database

---
+++ table_creations.sql
@@ -0,0 +1,24 @@
+CREATE TABLE owner ( owner_id SERIAL PRIMARY KEY,
+				     owner_name TEXT NOT NULL,
+					 owner_lang TEXT NOT NULL,
+				     owner_created TIMESTAMP WITH TIME ZONE,
+				     owner_createdby TEXT,
+				     owner_updated TIMESTAMP WITH TIME ZONE,
+				     owner_updatedby TEXT );
+
+CREATE TABLE pet ( pet_id SERIAL PRIMARY KEY,
+				   pet_name TEXT NOT NULL, 
+				   pet_owner_id INTEGER REFERENCES owner (owner_id),
+				   pet_type TEXT NOT NULL,
+				   pet_breed TEXT NOT NULL,
+				   pet_gender TEXT NOT NULL,
+				   pet_color TEXT NOT NULL, 
+				   pet_dob DATE,
+				   pet_current_weight NUMERIC );
+
+CREATE TABLE picture ( picture_id SERIAL PRIMARY KEY,
+					   picture_parent_type TEXT NOT NULL,
+					   picture_parent_id INTEGER NOT NULL,
+					   picture_primary BOOLEAN NOT NULL,
+					   picture_description TEXT NOT NULL,
+					   picture_source TEXT NOT NULL );
