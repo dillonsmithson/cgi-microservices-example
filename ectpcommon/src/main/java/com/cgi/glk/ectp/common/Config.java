1576e66389e5db9c73b4bf1a4417de9929d36d35
dillonsmithson <dillon.smithson1@gmail.com>
2022-03-14 14:10:57 -0500

    Updated AuthService in Orch, it was missing code for that was needed

--- ectpcommon/src/main/java/com/cgi/glk/ectp/common/Config.java
+++ ectpcommon/src/main/java/com/cgi/glk/ectp/common/Config.java
@@ -5,7 +5,9 @@ import com.auth0.jwt.JWTVerifier;
 import com.auth0.jwt.algorithms.Algorithm;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class Config {
 
     @Autowired private Properties properties;
